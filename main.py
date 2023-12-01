cbc0437643976906b0232cd1d1a1df2c106d8bde
lowkechika <jmtagare@gmail.com>
2023-12-01 13:34:20 +0300

    add wall-breaking funtionality

--- main.py
+++ main.py
@@ -2,24 +2,24 @@ import turtle
 from ball import Ball
 from paddle import Paddle
 from bricks import Brick
+import time
 
 window = turtle.Screen()
 window.setup(width=900, height=600)
 window.title('Breakout')
 
-window.listen()
-
 ball = Ball()
-ball.speed(0.7)
+ball.speed(0.05)
 paddle = Paddle()
 brick = Brick()
 
+# keybindings
+window.listen()
 window.onkey(paddle.move_right, 'Right')
 window.onkey(paddle.move_left, 'Left')
 
 
-def play_game():
-    ball.move()
+def detect_collision():
     if ball.xcor() > 430 or ball.xcor() < -430:
         ball.bounce_x()
     elif ball.ycor() > 280:
@@ -28,10 +28,25 @@ def play_game():
         ball.reset_()
     elif ball.distance(paddle) < 30:
         ball.bounce_y()
+    for block in brick.all_bricks:
+        if ball.distance(block) < 25:
+            try:
+                block.hideturtle()
+                brick.all_bricks.remove(block)
+                ball.bounce_y()
+            except ValueError:
+                pass
 
+
+brick.generate_bricks()
+brick.display_bricks()
 
 game_on = True
 while game_on:
-    play_game()
+    ball.move()
+    window.update()
+    time.sleep(0.001)
+    detect_collision()
+
 
 window.mainloop()
